17ec2c384eb000ec17cbcedf27b97167dcec3f44
Chris Duncan <chris@zoso.dev>
2025-10-15 09:29:34 -0700

    Update crypto calls to current function signatures.

--- src/ed25519_shim.c
+++ src/ed25519_shim.c
@@ -7,15 +7,15 @@ void ed25519_publickey(const libn_private_key_t privateKey,
     cx_ecfp_private_key_t sdkPrivateKey;
     cx_ecfp_public_key_t sdkPublicKey;
 
-    cx_ecfp_init_private_key(CX_CURVE_Ed25519,
+    cx_ecfp_init_private_key_no_throw(CX_CURVE_Ed25519,
         (uint8_t *)privateKey, sizeof(libn_private_key_t),
         &sdkPrivateKey);
-    cx_ecfp_init_public_key(CX_CURVE_Ed25519, NULL, 0, &sdkPublicKey);
+    cx_ecfp_init_public_key_no_throw(CX_CURVE_Ed25519, NULL, 0, &sdkPublicKey);
 
-    cx_ecfp_generate_pair2(CX_CURVE_Ed25519, &sdkPublicKey, &sdkPrivateKey, true, CX_BLAKE2B);
+    cx_ecfp_generate_pair2_no_throw(CX_CURVE_Ed25519, &sdkPublicKey, &sdkPrivateKey, true, CX_BLAKE2B);
     os_memset(&sdkPrivateKey, 0, sizeof(sdkPrivateKey));
 
-    cx_edward_compress_point(CX_CURVE_Ed25519, sdkPublicKey.W, sdkPublicKey.W_len);
+    cx_edwards_compress_point_no_throw(CX_CURVE_Ed25519, sdkPublicKey.W, sdkPublicKey.W_len);
     memmove(publicKey, sdkPublicKey.W+1, sizeof(libn_public_key_t));
 }
 
@@ -23,17 +23,17 @@ void ed25519_sign(const uint8_t *m, size_t mlen,
                   const libn_private_key_t privateKey,
                   libn_signature_t signature) {
     cx_ecfp_private_key_t sdkPrivateKey;
-    cx_ecfp_init_private_key(CX_CURVE_Ed25519,
+    cx_ecfp_init_private_key_no_throw(CX_CURVE_Ed25519,
         (uint8_t *)privateKey, sizeof(libn_private_key_t),
         &sdkPrivateKey);
 
-    cx_eddsa_sign(
+    cx_eddsa_sign_no_throw(
         &sdkPrivateKey,
-        0, CX_BLAKE2B,
-        (uint8_t *)m, mlen,
-        NULL, 0,
-        signature, sizeof(libn_signature_t),
-        0);
+        CX_BLAKE2B,
+        (uint8_t *)m,
+        mlen,
+        signature,
+        sizeof(libn_signature_t));
     os_memset(&sdkPrivateKey, 0, sizeof(sdkPrivateKey));
 }
 
@@ -41,17 +41,18 @@ int ed25519_sign_open(const uint8_t *m, size_t mlen,
                       const libn_public_key_t publicKey,
                       const libn_signature_t signature) {
     cx_ecfp_public_key_t sdkPublicKey;
-    cx_ecfp_init_public_key(CX_CURVE_Ed25519, NULL, 0, &sdkPublicKey);
+    cx_ecfp_init_public_key_no_throw(CX_CURVE_Ed25519, NULL, 0, &sdkPublicKey);
 
     sdkPublicKey.W[0] = 0x02;
     memmove(sdkPublicKey.W+1, publicKey, sizeof(libn_public_key_t));
-    cx_edward_decompress_point(CX_CURVE_Ed25519, sdkPublicKey.W, sdkPublicKey.W_len);
+    cx_edwards_decompress_point_no_throw(CX_CURVE_Ed25519, sdkPublicKey.W, sdkPublicKey.W_len);
     sdkPublicKey.W_len = 65;
 
-    return cx_eddsa_verify(
+    return cx_eddsa_verify_no_throw(
         &sdkPublicKey,
-        0, CX_BLAKE2B,
-        (uint8_t *)m, mlen,
-        NULL, 0,
-        (uint8_t *)signature, sizeof(libn_signature_t));
+        CX_BLAKE2B,
+        (uint8_t *)m,
+        mlen,
+        (uint8_t *)signature,
+        sizeof(libn_signature_t));
 }
