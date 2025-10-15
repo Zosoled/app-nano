76249f267783de969f8890ad8feb75192f96625e
Chris Duncan <chris@zoso.dev>
2025-10-14 23:40:27 -0700

    Fix blake2b shim errors. Update deprecated initializer call and remove its unused parameters. Update deprecated hash function calls.

--- src/blake2b_shim.c
+++ src/blake2b_shim.c
@@ -2,17 +2,16 @@
 #include "os.h"
 #include "cx.h"
 
-int blake2b_init(blake2b_ctx *ctx, size_t outlen,
-    const void *key, size_t keylen) {
-    cx_blake2b_init(ctx, outlen * 8);
+int blake2b_init(blake2b_ctx *ctx, size_t outlen) {
+    cx_blake2b_init_no_throw(ctx, outlen * 8);
     return 0;
 }
 
 void blake2b_update(blake2b_ctx *ctx,
     const void *in, size_t inlen) {
-    cx_hash(&ctx->header, 0, (void *)in, inlen, NULL, 0);
+    cx_hash_no_throw(&ctx->header, 0, (void *)in, inlen, NULL, 0);
 }
 
 void blake2b_final(blake2b_ctx *ctx, void *out) {
-    cx_hash(&ctx->header, CX_LAST, NULL, 0, out, ctx->ctx.outlen);
+    cx_hash_no_throw(&ctx->header, CX_LAST, NULL, 0, out, ctx->ctx.outlen);
 }
