d0a87bb84eee683b69e6c6001b4a291b51862651
Chris Duncan <chris@zoso.dev>
2025-11-03 14:39:10 -0800

    Update settings with real values.

--- src/globals.h
+++ src/globals.h
@@ -17,8 +17,7 @@ extern global_ctx_t G_context;
  * Global structure for NVM data storage.
  */
 typedef struct internal_storage_t {
-    uint8_t dummy1_allowed;
-    uint8_t dummy2_allowed;
+    uint8_t autoreceive_allowed;
     uint8_t initialized;
 } internal_storage_t;
 
--- src/ui/menu_nbgl.c
+++ src/ui/menu_nbgl.c
@@ -36,13 +36,13 @@ void app_quit(void) {
 //  -----------------------------------------------------------
 //  --------------------- SETTINGS MENU -----------------------
 //  -----------------------------------------------------------
-#define SETTING_INFO_NB 2
-static const char *const INFO_TYPES[SETTING_INFO_NB] = {"Version", "Developer"};
-static const char *const INFO_CONTENTS[SETTING_INFO_NB] = {APPVERSION, "Ledger"};
+#define SETTING_INFO_NB 3
+static const char *const INFO_TYPES[SETTING_INFO_NB] = {"Version", "Author", "Contributor"};
+static const char *const INFO_CONTENTS[SETTING_INFO_NB] = {APPVERSION, "Mart Roosma", "Chris Duncan"};
 
 // settings switches definitions
-enum { DUMMY_SWITCH_1_TOKEN = FIRST_USER_TOKEN, DUMMY_SWITCH_2_TOKEN };
-enum { DUMMY_SWITCH_1_ID = 0, DUMMY_SWITCH_2_ID, SETTINGS_SWITCHES_NB };
+enum { AUTORECEIVE_SWITCH_TOKEN = FIRST_USER_TOKEN };
+enum { AUTORECEIVE_SWITCH_ID = 0, SETTINGS_SWITCHES_NB };
 
 static nbgl_contentSwitch_t switches[SETTINGS_SWITCHES_NB] = {0};
 
@@ -69,14 +69,14 @@ static const nbgl_genericContents_t settingContents = {.callbackCallNeeded = fal
                                                        .nbContents = SETTING_CONTENTS_NB};
 
 // callback for setting warning choice
-static void review_warning_choice(bool confirm) {
+static void autoreceive_warning_choice(bool confirm) {
     uint8_t switch_value;
     if (confirm) {
         // toggle the switch value
-        switch_value = !N_storage.dummy2_allowed;
-        switches[DUMMY_SWITCH_2_ID].initState = (nbgl_state_t) switch_value;
+        switch_value = !N_storage.autoreceive_allowed;
+        switches[AUTORECEIVE_SWITCH_ID].initState = (nbgl_state_t) switch_value;
         // store the new setting value in NVM
-        nvm_write((void *) &N_storage.dummy2_allowed, &switch_value, 1);
+        nvm_write((void *) &N_storage.autoreceive_allowed, &switch_value, 1);
     }
 
     // Reset setting menu to the right page
@@ -96,32 +96,23 @@ static void controls_callback(int token, uint8_t index, int page) {
     initSettingPage = page;
 
     uint8_t switch_value;
-    if (token == DUMMY_SWITCH_1_TOKEN) {
-        // Dummy 1 switch touched
-        // toggle the switch value
-        switch_value = !N_storage.dummy1_allowed;
-        switches[DUMMY_SWITCH_1_ID].initState = (nbgl_state_t) switch_value;
-        // store the new setting value in NVM
-        nvm_write((void *) &N_storage.dummy1_allowed, &switch_value, 1);
-    } else if (token == DUMMY_SWITCH_2_TOKEN) {
-        // Dummy 2 switch touched
-
-        // in this example we display a warning when the user wants
-        // to activate the dummy 2 setting
-        if (!N_storage.dummy2_allowed) {
+    if (token == AUTORECEIVE_SWITCH_TOKEN) {
+        // Auto-receive switch touched, display a warning when the user wants
+        // to activate the setting
+        if (!N_storage.autoreceive_allowed) {
             // Display the warning message and ask the user to confirm
             nbgl_useCaseChoice(&ICON_APP_WARNING,
-                               "Dummy 2",
-                               "Are you sure to\nallow dummy 2\nin transactions?",
+                               "Auto-receive",
+                               "Inbound receivable transactions will be\nautomatically signed\nwithout user confirmation.",
                                "I understand, confirm",
                                "Cancel",
-                               review_warning_choice);
+                               autoreceive_warning_choice);
         } else {
             // toggle the switch value
-            switch_value = !N_storage.dummy2_allowed;
-            switches[DUMMY_SWITCH_2_ID].initState = (nbgl_state_t) switch_value;
+            switch_value = !N_storage.autoreceive_allowed;
+            switches[AUTORECEIVE_SWITCH_ID].initState = (nbgl_state_t) switch_value;
             // store the new setting value in NVM
-            nvm_write((void *) &N_storage.dummy2_allowed, &switch_value, 1);
+            nvm_write((void *) &N_storage.autoreceive_allowed, &switch_value, 1);
         }
     }
 }
@@ -129,20 +120,12 @@ static void controls_callback(int token, uint8_t index, int page) {
 // home page definition
 void ui_menu_main(void) {
     // Initialize switches data
-    switches[DUMMY_SWITCH_1_ID].initState = (nbgl_state_t) N_storage.dummy1_allowed;
-    switches[DUMMY_SWITCH_1_ID].text = "Dummy 1";
-    switches[DUMMY_SWITCH_1_ID].subText = "Allow dummy 1\nin transactions";
-    switches[DUMMY_SWITCH_1_ID].token = DUMMY_SWITCH_1_TOKEN;
+    switches[AUTORECEIVE_SWITCH_ID].initState = (nbgl_state_t) N_storage.autoreceive_allowed;
+    switches[AUTORECEIVE_SWITCH_ID].text = "Auto-receive";
+    switches[AUTORECEIVE_SWITCH_ID].subText = "Sign receivables without input";
+    switches[AUTORECEIVE_SWITCH_ID].token = AUTORECEIVE_SWITCH_TOKEN;
 #ifdef HAVE_PIEZO_SOUND
-    switches[DUMMY_SWITCH_1_ID].tuneId = TUNE_TAP_CASUAL;
-#endif
-
-    switches[DUMMY_SWITCH_2_ID].initState = (nbgl_state_t) N_storage.dummy2_allowed;
-    switches[DUMMY_SWITCH_2_ID].text = "Dummy 2";
-    switches[DUMMY_SWITCH_2_ID].subText = "Allow dummy 2\nin transactions";
-    switches[DUMMY_SWITCH_2_ID].token = DUMMY_SWITCH_2_TOKEN;
-#ifdef HAVE_PIEZO_SOUND
-    switches[DUMMY_SWITCH_2_ID].tuneId = TUNE_TAP_CASUAL;
+    switches[AUTORECEIVE_SWITCH_ID].tuneId = TUNE_TAP_CASUAL;
 #endif
 
     nbgl_useCaseHomeAndSettings(APPNAME,
